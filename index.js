f4ed80a182c7a299c235e1ab6553cef2b245b5d9
DiegOrtega <nemesisrda@gmail.com>
2015-07-04 22:53:22 -0500

    prubas locales

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ app.set('port', (5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '\view\pages');
+app.set('views', __dirname + '/view/pages');
 app.set('partials', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('views\pages\index.ejs')
+  response.render('Users/DiegOrtega/node-js-getting-started/views/pages/index.ejs');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Listening on ', app.get('port'));
 });
+
 
 
