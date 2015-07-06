5023f1f5fc7dfa550bad69421c689066e4a84969
DiegOrtega <nemesisrda@gmail.com>
2015-07-06 15:56:37 -0500

    prueba en index.js

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('pages/index.ejs')
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+/*
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running on port:" + app.get('port'))
+})
