5e99180d7bf7c878a20c37cb2ba102f191389485
DiegOrtega <nemesisrda@gmail.com>
2015-07-06 15:09:11 -0500

    Reparando errores en index.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
+
 var express = require('express');
 var app = express();
 
-app.set('port', (5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/view/pages');
-app.set('partials', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('Users/DiegOrtega/node-js-getting-started/views/pages/index.ejs');
+  response.render('pages/index')
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Listening on ', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
-
-
-
