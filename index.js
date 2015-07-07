cd12df7dd3e12e333d2707236d3d0bd8270eea3e
DiegOrtega <nemesisrda@gmail.com>
2015-07-06 22:22:22 -0500

    cambios

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/db', function (request, response) {
-  pg.connect(DATABASE_URL, function(err, client, done) {
+  pg.connect(procces.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
