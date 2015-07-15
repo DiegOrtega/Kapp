feb1ed247a492e8bb20504cdfb98094ca04357ee
DiegOrtega <nemesisrda@gmail.com>
2015-07-15 15:29:43 -0500

    cambios en index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 var pg = require('pg');
@@ -29,6 +29,11 @@ app.get('/db', function (request, response) {
 app.get('/', function(request, response) {
   response.render('pages/index')
 });
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
