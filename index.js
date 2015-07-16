630414f77004371c9665ec67d83058035ef1b955
DiegOrtega <nemesisrda@gmail.com>
2015-07-15 19:40:30 -0500

    arreglos

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var app = express();
 var pg = require('pg');
+var app = express();
+
 
 
 app.set('port', (process.env.PORT || 5001));
@@ -13,8 +14,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/db', function (request, response) {
-  pg.connect(procces.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+  pg.connect(procces.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM test_table', function (err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
