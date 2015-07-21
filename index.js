0fdcd4ee68a41994bdbaaccd597292d68bd1b83d
DiegOrtega <nemesisrda@gmail.com>
2015-07-20 21:33:09 -0500

    compilando página

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
-  //, routes = require('node-js-getting-started')
- // , http = require('http')
- // , path = require('path');
 var pg = require('pg');
 var query = require("pg-query");
 var conString = process.env.DATABASE_URL || "postgres://mpjcvtjxocsmeb:gruLA0ckuOeqIRfkRyHDp9Vre9@ec2-54-204-27-193.compute-1.amazonaws.com:5432/d63j6ljg1re6ac";
-//var Sequelize = require('sequelize');
-//var sequelize = new Sequelize('postgres://mpjcvtjxocsmeb:gruLA0ckuOeqIRfkRyHDp9Vre9@ec2-54-204-27-193.compute-1.amazonaws.com:5432/d63j6ljg1re6ac');
+
 var app = express();
 var client = new pg.Client(conString);
-//client.connect();
 
-app.set('port', process.env.PORT || 5023);
+
+app.set('port', (process.env.PORT || 5023));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
