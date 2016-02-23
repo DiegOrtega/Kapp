0f721d5964701644f5daecd12a8f76d6b9a23d58
DiegOrtega <nemesisrda@gmail.com>
2016-02-23 14:01:00 -0600

    Avances has el 23 de feb

--- public/javascripts/index-view.js
+++ public/javascripts/index-view.js
@@ -1,27 +1,41 @@
 $(document).ready(function() {
   /* Push the body and the nav over by 285px over */
-  $('.icon-menu i').click(function() {
+  $('.icon-menu').click(function() {
     $('.menu').animate({
       left: "0px"
     }, 200);
 
     $('body').animate({
-      left: "285px"
+      left: "300px",
+      marginRight: "300px"
     }, 200);
+
+
+    $('.icon-menu').animate({
+      left:"-285px"
+    },200);
+
+
   });
 
   
   $('.menu').mouseleave(function() {
     $('.menu').animate({
 
-
       left: "-285px"
     }, 200);
 
     $('body').animate({
-      left: "0px"
+      left: "0px",
+      marginRight: "0px"
     }, 200);
+
+    $('.icon-menu').animate({
+      left:"0px"
+    },200);
+
   });
+  
 
 });
 
