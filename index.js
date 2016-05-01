f0c3ed85b01637bff5b9e4ea317a5f77e4e08636
DiegOrtega <nemesisrda@gmail.com>
2016-05-01 18:23:45 -0500

    Forma de registro v1.

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
+/*var pg = require('pg');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
+*/
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +36,10 @@ app.set('view engine', 'ejs');
 
 app.get('/index', function(request, response) {
   response.render('pages/index');
+});
+
+app.get('/registro', function(request, response) {
+  response.render('pages/registro');
 });
 
 app.get('/', function(request, response) {
