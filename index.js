065f832c80a734dec5b12ee3f3cdab78b7db61d8
DiegOrtega <nemesisrda@gmail.com>
2016-08-01 21:53:29 -0500

    neo4j ref BOLT protocol corr6

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 5000));
 
-var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
-var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
-var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+var graphenedbURL = process.env.GRAPHENEDB_MAROON_BOLT_URL;
+var graphenedbUser = process.env.GRAPHENEDB_MAROON_BOLT_USER;
+var graphenedbPass = process.env.GRAPHENEDB_MAROON_BOLT_PASSWORD;
 
 //var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
 var driver = neo4j.driver('bolt://localhost' || graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass) || neo4j.auth.basic('neo4j','tractus0'));
