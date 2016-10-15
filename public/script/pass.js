6623f39d96a1ace36f433562e38b383183c70f9a
DiegOrtega <nemesisrda@gmail.com>
2016-10-15 17:21:06 -0500

    Actualización de interfaz de registro: -Aviso para completar campos, referencia color del botón de registro

--- public/script/pass.js
+++ public/script/pass.js
@@ -2,7 +2,9 @@ document.getElementById("b_register").disabled = true;
 var input = document.forms["form_register"];
 var v0 = input.elements[0].value, v1 = input.elements[1].value, v2 = input.elements[2].value, v3 = input.elements[3].value, v4 = input.elements[4].value, v5 = input.elements[5].value, v6 = input.elements[6].value, v7 = input.elements[7].value;
 
-//console.log("el valor de 0 es "+ v0);
+document.getElementById("b_register").style.background="gray";
+document.getElementById("b_register").style.backgroundColor="gray";
+document.getElementById("b_register").style.color="black";
 
 function checkPasswordMatch() {
     var password = $("#contraseña").val(), confirmPassword = $("#confirmacion_contraseña").val();
@@ -12,7 +14,6 @@ function checkPasswordMatch() {
         if (password.length > 6) {
             $("#divCheckPasswordMatch2").html("");
             return true  
-            //confirm_password.setCustomValidity("Las contraseñas no coinciden");
         } else {
             $("#divCheckPasswordMatch2").html("La contraseña es demasiado corta");
             return false
@@ -22,7 +23,6 @@ function checkPasswordMatch() {
         if (password.length > 6) {
             $("#divCheckPasswordMatch2").html("");
             return true;  
-            //confirm_password.setCustomValidity("Las contraseñas no coinciden");
         } else {
             $("#divCheckPasswordMatch2").html("La contraseña es demasiado corta");
             return false
@@ -35,7 +35,7 @@ function checkPasswordMatch() {
           
         if (val === false) {    
             $("#emailCheck").html("El email no es válido");
-            //$("#divCheckPasswordMatch").html("suck!");
+           
             console.log("email " + val);
         } else {
          $("#emailCheck").html("");   
@@ -48,8 +48,12 @@ function checkPasswordMatch() {
         var val = validator.isEmail(input.elements[3].value);
         console.log("checkboxes = " + checkboxes);
     if(input.elements[0].value != v0 && input.elements[1].value != v1 && input.elements[2].value != v2 && input.elements[3].value != v3 && input.elements[4].value != v4 && input.elements[5].value != v5 && input.elements[6].value != v6 && input.elements[7].value != v7 && checkboxes === true && checkPasswordMatch() === true && val === true){
+        
         document.getElementById("b_register").disabled=false;
+        document.getElementById("b_register").style.backgroundColor="#58135D";
+        document.getElementById("b_register").style.color="white";
         console.log("listones!")
+        
     }else{
         document.getElementById("b_register").disabled=true;
         console.log("falta llenar valores");
@@ -64,4 +68,5 @@ function checkPasswordMatch() {
         $("#contraseña, #confirmacion_contraseña").focusout(checkPasswordMatch);
         $("#form_register").keyup(checkValues);
         $("#email").focusout(email_check);
+        //$("b_register").click(email_check);
     });
